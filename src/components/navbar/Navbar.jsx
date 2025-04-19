9aafb03eeccae611eb5541f80c542aabcfa60a30
yashiikaaaa <yashika@truestate.in>
2025-04-19 18:21:32 +0530

    number

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -124,10 +124,10 @@ export const Navbar = ({ sitevisitmodal, setSiteVisitModal, setContactModal }) =
           {/* Call Button */}
           <div className="hidden lg:flex items-center">
             <a
-              href="tel:+918431755048"
+              href="tel:+919353329893"
               className="flex items-center bg-PrestigeBrown text-white px-4 py-2 rounded-lg shadow-lg hover:bg-opacity-90 transition"
             >
-              <Phone className="w-5 h-5 mr-2" /> 8431755048
+              <Phone className="w-5 h-5 mr-2" /> 9353329893
             </a>
           </div>
         </div>
